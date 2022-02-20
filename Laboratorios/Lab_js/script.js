d63f73672b9282413908c52447eb0b9c3cce6d77
FerminMendez <MY_NAME@example.com>
2022-02-20 17:51:10 -0600

    prompt problema 1

--- Laboratorios/Lab_js/script.js
+++ Laboratorios/Lab_js/script.js
@@ -1,3 +1,4 @@
 function outp1() {
-    document.getElementById("output_p1").innerHTML="Esta sería la respuesta al problema 1"
+    let num=window.prompt("Ingresa un número");
+    document.getElementById("output_p1").innerHTML="Esta sería la respuesta al problema 1. El número que ingresaste es: " + num ;
 }
