86ecb21277af784ecb11fc1c59fd72039e4dcee5
FerminMendez <MY_NAME@example.com>
2022-02-20 18:28:23 -0600

    p1 resuelto

--- Laboratorios/Lab_js/script.js
+++ Laboratorios/Lab_js/script.js
@@ -1,4 +1,15 @@
 function outp1() {
-    let num=window.prompt("Ingresa un número");
-    document.getElementById("output_p1").innerHTML="Esta sería la respuesta al problema 1. El número que ingresaste es: " + num ;
+    let num = window.prompt("Ingresa un número");
+    let n1="";
+    let n2="";
+    let n3="";
+    for (let i=1; i<=num; i++){ 
+        n1 += "<td>" + i + "</td>";
+        n2 += "<td>" + i*i + "</td>";
+        n3 += "<td>" + i*i*i + "</td>";
+        }
+
+    let mytable = "<table><tr>"+ n1 + "<tr>"+ "<tr>" + n2 + "<tr>"+ "<tr>" + n3 + "</tr></table>";
+    console.log(mytable);
+    document.getElementById("output_p1").innerHTML = mytable;
 }
