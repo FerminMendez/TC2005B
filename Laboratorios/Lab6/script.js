cc13d32bee19df9465e80f970ff8534245e32332
FerminMendez <MY_NAME@example.com>
2022-02-25 20:41:08 -0600

    Validación de passwords

--- Laboratorios/Lab6/script.js
+++ Laboratorios/Lab6/script.js
@@ -1,5 +1,24 @@
-let input_jugador = document.getElementById("jugador");
-input_jugador.onkeyup=()=>{
-    let div_respuesta= documet.getElementById("respuesta");
-    div_respuesta.innerHTML="Tu jugador favorito es"+ input_jugador.value;
+
+
+let input_user = document.getElementById("user");
+let outp = document.getElementById("out");
+
+input_user.onkeyup = () => {
+    outp.innerHTML = " Tu usuario ingresado es " + input_user.value;
+}
+
+let input_password = document.getElementById("password");
+input_password.onkeyup = () => {
+    outp.innerHTML = " Tu contraseña es " + input_password.value;
+}
+
+let boton_validar= document.getElementById("validate");
+
+boton_validar.onclick=()=>{
+    if(input_password.value=="123" && input_user.value=="us1"){
+        alert("Bienvenido");
+    }
+    else{
+        alert("No te conozco")
+    }
 }
