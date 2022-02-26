447a2a9fb34c7e2195dbc06c586e11ba57b0d0a7
FerminMendez <MY_NAME@example.com>
2022-02-26 08:38:29 -0600

    Despliega un producto correctamente

--- Laboratorios/Lab6/script.js
+++ Laboratorios/Lab6/script.js
@@ -22,3 +22,20 @@ boton_validar.onclick=()=>{
         alert("No te conozco")
     }
 }
+
+// Pagina de ventas
+let imgref=['"https://www.presentsofmind.com.au/wp-content/uploads/2021/11/ac22086f-17a5-4db3-86b8-e9a1c7c3a0be.jpg"',"https://www.presentsofmind.com.au/wp-content/uploads/2021/11/ac22086f-17a5-4db3-86b8-e9a1c7c3a0be.jpg","https://www.presentsofmind.com.au/wp-content/uploads/2021/11/ac22086f-17a5-4db3-86b8-e9a1c7c3a0be.jpg"]
+let product_name=["Ice cards","Fire cards","Basic cards"]
+let stock=[150,58,49];
+let costo_producto=[180,150,210];
+boton_ver_oferta=document.getElementById("ver_oferta");
+boton_ver_oferta.onclick=()=>{
+    document.getElementById("im"+1).innerHTML='<img src='+imgref[0]+'alt="ice cards">';
+    document.getElementById("name"+1).innerHTML=product_name[0];
+    document.getElementById("costo"+1).innerHTML="Costo: "+costo_producto[0];
+    document.getElementById("stock"+1).innerHTML="Unidades disponibles: "+stock[0];
+    let num_label='<label for="quantity">Unidades </label>';
+    let num_input='<input type="number" id="input_num'+1+'" name="Unidades" min="0" max="'+5+'" class="text-black">'
+    document.getElementById("piezas_compra1").innerHTML=num_label+ num_input;
+
+}
