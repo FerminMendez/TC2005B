6c07a125de7d739a4a43a4481e2e2d51b3e23967
FerminMendez <MY_NAME@example.com>
2022-02-27 17:53:27 -0600

    Servidor funcionando

--- Laboratorios/Lab8_backend/pruebas_node/frutas.js
+++ Laboratorios/Lab8_backend/pruebas_node/frutas.js
@@ -2,32 +2,32 @@ const exp = require('constants');
 
 console.log("Usted ha invocado al poderosisimo archivo de frutas.js")
 
-function avarage(array){
-    let res=0;
+function avarage(array) {
+    let res = 0;
     array.forEach(element => {
-        res+=element;
+        res += element;
     });
-    res=res/array.length;
+    res = res / array.length;
     return res;
 }
 
 
-function create_txt(text_string){
+function create_txt(text_string) {
     const filesystem = require('fs');
     filesystem.writeFileSync('nuevo.txt', text_string);
     return "Archivo creado correctamente";
 }
 
 
-function watermelon(w){
-    if(w%4==0){
-       return "YES";
+function watermelon(w) {
+    if (w % 4 == 0) {
+        return "YES";
     }
-    else{
+    else {
         return "NO";
     }
 }
 
 exports.avarage = avarage;
-exports.create_txt=create_txt;
-exports.watermelon=watermelon;
+exports.create_txt = create_txt;
+exports.watermelon = watermelon;
