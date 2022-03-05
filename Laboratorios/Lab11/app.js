c0335ddf6403c8037837e548d3ad7d9e0b2440e2
FerminMendez <MY_NAME@example.com>
2022-03-05 15:38:08 -0600

    Nodemon include

--- Laboratorios/Lab11/app.js
+++ Laboratorios/Lab11/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const path= require('path');
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 //Middleword
 
