a42b05e90e6c4ab406ac7e16c38304af59c96799
FerminMendez <MY_NAME@example.com>
2022-03-07 09:26:14 -0600

    lab 12 sin terminar

--- Laboratorios/Lab12_html_dinamico/app.js
+++ Laboratorios/Lab12_html_dinamico/app.js
@@ -1,11 +1,11 @@
 
-
-
 console.log('Hellooo');
 
 const express= require('express');
 const { listenerCount } = require('process');
 const app= express();
+const path = require('path');
+
 
 app.get('/',(req,res)=>{
     res.send("Hola, aqui andamos");
